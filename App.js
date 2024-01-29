21ecd9f114cd4d7ac71067159c916db0f27ca24a
GOKULKRISHNA-R <gokulkrishna.raju@surveysparrow.com>
2024-01-29 10:13:54 +0530

    29 Jan - Lap Change

---
+++ App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {StyleSheet, Button, View} from 'react-native';
+import {
+  SurveySparrow,
+  invokeSurveySparrow,
+  onSurveyResponseListener,
+} from 'surveysparrow-react-native-sdk';
+
+const App = () => {
+  React.useEffect(() => {
+    const sub = onSurveyResponseListener.addListener(
+      'onSurveyResponse',
+      data => {
+        console.log('survey response is', data);
+      },
+    );
+    return () => sub.remove();
+  }, []);
+
+  const surveyConfig = {
+    domain: 'gokulkrishnaraju1183.surveysparrow.com',
+    token: 'tt-bwBtpzVStCVCgbQBh9wg94',
+    surveyType: 'survey-type',
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button
+        title="Open Survey"
+        onPress={() => {
+          invokeSurveySparrow(surveyConfig);
+        }}
+      />
+      <SurveySparrow
+        styles={styles.box}
+        config={{
+          domain: 'gokulkrishnaraju1183.surveysparrow.com',
+          token: 'tt-bwBtpzVStCVCgbQBh9wg94',
+          surveyType: 'survey-type',
+          customParams: [
+            {name: 'testname', value: 'custom value 10'},
+            {name: 'testname2', value: 'custom value 2'},
+          ],
+        }}
+        onSurveyComplete={data => {
+          console.log('survey response from embed survey is', data);
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    width: '100%',
+    height: 400,
+  },
+});
+
+export default App;
